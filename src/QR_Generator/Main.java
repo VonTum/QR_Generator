26fa20af9b753288b5858093508181fc0b2af7e7
lennartVH01 <lennart.vanhirtum@gmail.com>
2015-03-02 14:13:33 +0100

    Added Comments and created first classes

---
+++ src/QR_Generator/Main.java
@@ -0,0 +1,8 @@
+package QR_Generator;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Convert Code
+		// TODO Initialise Screen
+	}
+}
---
+++ src/QR_Generator/Screen.java
@@ -0,0 +1,15 @@
+package QR_Generator;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class Screen extends JPanel{
+	public boolean[][] code;
+	public Screen(boolean[][] code){
+		// TODO Create initialisation method and screen size
+	}
+	public void paintComponent(Graphics g){
+		// TODO Draw QR_Code
+	}
+}
