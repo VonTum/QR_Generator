b51d23988b1bc3cbcc69c55f2ca6116d748a46f1
lennartVH01 <lennart.vanhirtum@gmail.com>
2015-03-11 11:15:56 +0100

    created screen setup and first steps for scrolling

--- src/QR_Generator/Main.java
+++ src/QR_Generator/Main.java
@@ -1,8 +1,37 @@
 package QR_Generator;
 
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+
+import javax.swing.JFrame;
+
+
 public class Main {
+	public static String input;
+	public static boolean[][] code;
 	public static void main(String[] args) {
+		input = args[0];
+		
+		code = new boolean[][] {{true, false, true, true},{false, true, true, true},{true, true, false, false},{false, true, false, true}};
+		
 		// TODO Convert Code
-		// TODO Initialise Screen
+		// Generate QR Code here, store in code
+		
+		
+		//Creating screen
+		JFrame f = new JFrame(input); // sets title of frame to inputed string
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		Screen s = new Screen(code);
+		f.add(s);
+		f.pack();
+		
+		f.addMouseWheelListener(new MouseWheelListener() {
+			public void mouseWheelMoved(MouseWheelEvent scrollEvent) {
+				System.out.println(scrollEvent.getPreciseWheelRotation());
+			}
+		});
+		
+		
 	}
 }
--- src/QR_Generator/Screen.java
+++ src/QR_Generator/Screen.java
@@ -1,15 +1,29 @@
 package QR_Generator;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
 public class Screen extends JPanel{
+	public static final int pxl_height = 32;
+	public static final int pxl_width = 32;
+	
+	
+	
 	public boolean[][] code;
 	public Screen(boolean[][] code){
+		this.code = code;
+		setPreferredSize(new Dimension(code.length * pxl_height, code[0].length * pxl_width));
 		// TODO Create initialisation method and screen size
 	}
 	public void paintComponent(Graphics g){
-		// TODO Draw QR_Code
+		for(int y = 0; y < code.length; y++){
+			for(int x = 0; x < code[0].length; x++){
+				if(code[x][y]) g.fillRect(x * pxl_width, y * pxl_height, pxl_width, pxl_height);
+			}
+		}
 	}
+	
 }
